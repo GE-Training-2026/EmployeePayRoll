bc9d61277dffed148e39bf82ce985dc2dfd547d9
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-10 01:13:39 +0530

    [Jay]: introduce EmployeePayrollDTO for request handling

--- src/main/java/com/gevernova/EmployeePayRoll/dto/EmployeePayRollDTO.java
+++ src/main/java/com/gevernova/EmployeePayRoll/dto/EmployeePayRollDTO.java
@@ -1,4 +1,17 @@
 package com.gevernova.EmployeePayRoll.dto;
 
+import lombok.*;
+
+/**
+ * DTO used to accept request data from client
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
 public class EmployeePayRollDTO {
+    private String name;
+    private double salary;
+    private String department;
 }
