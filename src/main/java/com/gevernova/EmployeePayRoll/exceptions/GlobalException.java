1e9c9b2cac17487cb9b7ef39380b69226a201079
Jay Vashishtha <jayvashishtha@Jays-MacBook-Air.local>
2026-02-10 01:33:27 +0530

    [Jay]: feat(exception): add global exception handling

--- src/main/java/com/gevernova/EmployeePayRoll/exceptions/GlobalException.java
+++ src/main/java/com/gevernova/EmployeePayRoll/exceptions/GlobalException.java
@@ -1,4 +1,16 @@
 package com.gevernova.EmployeePayRoll.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalException {
+
+    @ExceptionHandler(IDNotFound.class)
+    public ResponseEntity<String> throwError(IDNotFound idNotFound){
+        return new ResponseEntity<>("id not found ",HttpStatus.NOT_FOUND);
+    }
 }
